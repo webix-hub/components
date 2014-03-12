2bf602148f1227724fb6d3182d53e85ea59f38c1
Maksim Kozhukh <mkozhukh@ya.ru>
2014-03-12 15:39:02 +0300

    [add] config.tabs for dhx-scheduler

--- scheduler/scheduler.js
+++ scheduler/scheduler.js
@@ -1,10 +1,27 @@
 webix.protoUI({
 	name:"dhx-scheduler",
-	$init:function(){
-		this.$view.innerHTML = "<div class='dhx_cal_container' style='width:100%; height:100%;'><div class='dhx_cal_navline'><div class='dhx_cal_prev_button'>&nbsp;</div><div class='dhx_cal_next_button'>&nbsp;</div><div class='dhx_cal_today_button'></div><div class='dhx_cal_date'></div><div class='dhx_cal_tab dhx_cal_tab_first' name='day_tab' style='left:14px;'></div><div class='dhx_cal_tab' name='week_tab' style='left:75px;'></div><div class='dhx_cal_tab dhx_cal_tab_last' name='month_tab' style='left:136px;'></div></div><div class='dhx_cal_header'></div><div class='dhx_cal_data'></div></div>";
-		//because we are not messing with resize model
-		//if setSize will be implemented - below line can be replaced with webix.ready
-		webix.delay(webix.bind(this._render_once, this));
+	defaults:{
+		tabs:["day", "week", "month"]
+	},
+	$init:function(config){
+		this.$ready.push(function(){
+			var tabs = this.config.tabs;
+
+			var html = ["<div class='dhx_cal_container' style='width:100%; height:100%;'><div class='dhx_cal_navline'><div class='dhx_cal_prev_button'>&nbsp;</div><div class='dhx_cal_next_button'>&nbsp;</div><div class='dhx_cal_today_button'></div><div class='dhx_cal_date'></div>"];
+			if (tabs)
+				for (var i=0; i<tabs.length; i++)
+					html.push("<div class='dhx_cal_tab" +
+						((i===0)?" dhx_cal_tab_first":"") +
+						((i==tabs.length-1)?" dhx_cal_tab_last":"") +
+						"' name='"+tabs[i]+"_tab' ></div>");
+			html.push("</div><div class='dhx_cal_header'></div><div class='dhx_cal_data'></div></div>");
+
+			this.$view.innerHTML = html.join("");
+
+			//because we are not messing with resize model
+			//if setSize will be implemented - below line can be replaced with webix.ready
+			webix.delay(webix.bind(this._render_once, this));
+		});
 	},
 	_render_once:function(){
 		webix.require("scheduler/dhtmlxscheduler.css");
