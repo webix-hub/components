dd5b3302999e98680c48374218dbce3180a41798
Zwillinge <zwilling@webix.com>
2014-06-16 16:50:44 +0200

    [update] gridsuggest and datasuggest as component editors

--- datasuggest/datasuggest.js
+++ datasuggest/datasuggest.js
@@ -7,6 +7,17 @@ webix.protoUI({
 		body:{
 			xCount:3,
 			autoheight:true,
+			select:true
 		}
+	},
+	$init:function(){
+		this.$ready.push(this._first_render)	
+	},
+	_first_render:function(){
+		this.attachEvent('onValueSuggest', function(data){
+           	webix.delay(function(){
+                webix.callEvent("onEditEnd",[]);
+            });
+        });
 	}
 }, webix.ui.suggest);
