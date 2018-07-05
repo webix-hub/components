b8646857e0ed28c3d7cafddaf6b656ebb6b53799
listopad <olistopad@xbsoftware.com>
2018-07-05 19:50:34 +0300

    [update] justGage cdn

--- justgage/justgage.js
+++ justgage/justgage.js
@@ -2,27 +2,49 @@ webix.protoUI({
     name:'justgage-chart',
     $init:function(config){
         this._waitChart = webix.promise.defer();
-        this.$ready.push(this._render_after_load);
+        this.$ready.push(this._request_and_render);
     },
     defaults:{
         minHeight:100,
-        minWidth:200
+        minWidth:200,
+    },
+    _request_and_render:function(){
+
+        if (this.config.cdn === false){
+            // justGage uses document.getElementById, so ensure the rendering is finished
+            webix.delay(webix.bind(this._render_after_load, this));
+            return;
+        };
+
+        var cdn = this.config.cdn ? this.config.cdn : "https://cdnjs.cloudflare.com/ajax/libs/justgage/1.2.9";
+
+        var sources = [];
+
+        sources.push(cdn+"/justgage.js");
+        // raphael version included in the main justGage package is named differently
+        // while cdnjs stores raphael in its own repo
+        if (this.config.cdn)
+            sources.push(cdn+"/raphael-2.1.4.min.js");
+        else
+            sources.push("https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.4/raphael-min.js");
+
+        webix.require(sources)
+        .then(webix.bind( this._render_after_load, this ))
+        .catch(function(e){
+            console.log(e);
+        });
     },
     _render_after_load:function(){
-        webix.require([
-            "justgage/raphael.js",
-            "justgage/justgage.js?1"
-        ],function(){
-                var temp_id = this.config.id+"_"+webix.uid();
+        var temp_id = this.config.id+"_"+webix.uid();
 
-                this.$view.innerHTML= "<div id='"+temp_id+"' style='width:100%; height:100%;'></div>";
-                var gage_config = webix.extend({}, this.config);
-                gage_config.id = temp_id;
-                gage_config.relativeGaugeSize = true;
+        this.$view.innerHTML= "<div id='"+temp_id+"' style='width:100%; height:100%;'></div>";
+        var gage_config = webix.extend({}, this.config);
+        gage_config.id = temp_id;
+        gage_config.relativeGaugeSize = true;
 
-                this._chart = new JustGage(gage_config);
-                this._waitChart.resolve(this._chart);
-        },this);
+        this._chart = new JustGage(gage_config);
+
+        this._waitChart.resolve(this._chart);
     },
     setValue:function(value){
         this.config.value=value;
