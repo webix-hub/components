6d50e629f3ecc2a59dc8a71ac531f8e6b3f4b31d
listopad <olistopad@xbsoftware.com>
2018-07-09 13:25:21 +0300

    [update] raphael cdn update

--- raphael/raphael.js
+++ raphael/raphael.js
@@ -15,22 +15,36 @@ webix.protoUI({
 			} else this._ready_awaits = 1;
 		});
 
-		webix.delay(webix.bind(this._render_once, this));
+		webix.delay(this._require_once, this);
+	},
+	_require_once:function(){
+		if (this.config.cdn === false){
+			this._render_once();
+			return;
+		};
+
+		var cdn = this.config.cdn ? this.config.cdn : "https://cdnjs.cloudflare.com/ajax/libs/raphael/2.2.7";
+
+		webix.require([
+			cdn + "/raphael.min.js"
+		])
+		.then(webix.bind(this._render_once, this))
+		.catch(function(e){
+			console.log(e);
+		});	
 	},
 	_render_once:function(){
-		webix.require("raphael/raphael.js",function(first_init){
-			this._chart = Raphael(this.$view, this.$width, this.$height);
-			
-			if (this.config.init)
-				this.config.init.call(this);
-			if (this._ready_awaits == 1 && this.config.ready){
-				this.config.ready.call(this, this.data);
-				this._ready_awaits = 3;
-			} else
-				this._ready_awaits = 2;
+		this._chart = Raphael(this.$view, this.$width, this.$height);
+		
+		if (this.config.init)
+			this.config.init.call(this);
+		if (this._ready_awaits == 1 && this.config.ready){
+			this.config.ready.call(this, this.data);
+			this._ready_awaits = 3;
+		} else
+			this._ready_awaits = 2;
 
-			this._waitChart.resolve(this._chart);
-		}, this);
+		this._waitChart.resolve(this._chart);
 	},
 	$setSize:function(x,y){
 		if (webix.ui.view.prototype.$setSize.call(this,x,y)){
