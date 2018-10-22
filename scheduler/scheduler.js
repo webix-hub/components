152f8381cc94c85d0733b17277a1a6533e6222b9
listopad <olistopad@xbsoftware.com>
2018-10-22 16:20:19 +0300

    [fix] http path to https

--- scheduler/scheduler.js
+++ scheduler/scheduler.js
@@ -55,7 +55,7 @@ webix.protoUI({
 			return;
 		};
 
-		this._cdn = this._cdn || "http://cdn.dhtmlx.com/scheduler/5.0";
+		this._cdn = this._cdn || "https://cdn.dhtmlx.com/scheduler/5.0";
 		var sources = [
 			this._cdn+"/dhtmlxscheduler"+skin+".css",
 			this._cdn+"/dhtmlxscheduler.js"			
