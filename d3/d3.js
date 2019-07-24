1e2198d347ac9ae11fe5d407547fc3f368e633b7
listopad <olistopad@xbsoftware.com>
2019-07-24 16:33:41 +0300

    [update] getChart for d3

--- d3/d3.js
+++ d3/d3.js
@@ -5,17 +5,7 @@ webix.protoUI({
 		version:"5"
 	},
 	$init:function(){
-		this._ready_awaits = 0;
-
-		this.attachEvent("onAfterLoad", function(){
-			if (this._ready_awaits == 2){
-				if (this.config.ready){
-					this.config.ready.call(this, this.data);
-					this._ready_awaits = 3;
-				}
-			} else this._ready_awaits = 1;
-		});
-
+		this._waitContent = webix.promise.defer();
 		webix.delay(this._render_once, this);
 	},
 	_render_once:function(){
@@ -33,26 +23,36 @@ webix.protoUI({
 		var source = cdn+"/d3.v"+version+isMin+".js";
 		
 		webix.require([ source ])
-		.then( webix.bind(this._set_ready_awaits, this) )
+		.then( webix.bind(this._finalize_init, this) )
 		.catch(function(e){
 			console.log(e);
-		});	;
+		});
 	},
-	_set_ready_awaits:function(first_init){
+	_finalize_init:function(){
+		this._waitContent.resolve(d3.select(this.$view));
 		if (this.config.init)
 			this.config.init.call(this);
-		if (this._ready_awaits == 1 && this.config.ready){
+		this._data_ready();
+	},
+	_data_ready:function(){
+		webix.promise.all([
+			this._waitContent,
+			this.waitData
+		])
+		.then( webix.bind(this._render_data, this) );
+	},
+	_render_data:function(){
+		if (this.config.ready){
+			this.$view.innerHTML = "";
 			this.config.ready.call(this, this.data);
-			this._ready_awaits = 3;
-		} else 
-			this._ready_awaits = 2;
+		}
 	},
 	$setSize:function(x,y){
 		if (webix.ui.view.prototype.$setSize.call(this,x,y)){
-			if (this._ready_awaits == 3 && this.config.resize){
-				this.$view.innerHTML = "";
-				this.config.ready.call(this, this.data);
-			}
+			this._data_ready();
 		}
+	},
+	getChart:function(wait){
+		return wait ? this._waitContent : d3.select(this.$view)
 	}
 }, webix.AtomDataLoader, webix.EventSystem, webix.ui.view );
