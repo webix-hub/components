d4352a868ababf15faa8c7b0ee6d543dd0ae8c5a
listopad <olistopad@xbsoftware.com>
2019-10-03 20:37:03 +0300

    [update] highcharts version update (7.2.0)

--- highcharts/hcharts.js
+++ highcharts/hcharts.js
@@ -18,7 +18,7 @@ webix.protoUI({
 			return;
 		};
 
-		var cdn = c.cdn || "https://code.highcharts.com/7.0.3";		
+		var cdn = c.cdn || "https://code.highcharts.com/7.2.0";		
 
 		var sources = [];
 			
