f57ce6a776ebbef4540a0025e9c3ca77141e29a4
listopad <olistopad@xbsoftware.com>
2019-10-18 15:29:36 +0300

    [update] codemirror code highlighting: improved support for complex languages

--- codemirror/codemirror.js
+++ codemirror/codemirror.js
@@ -1,19 +1,34 @@
 webix.protoUI({
-	name:"codemirror-editor",
-	defaults:{
-		mode:"javascript",
-		lineNumbers:true,
-		matchBrackets:true,
-		theme:"default"
+	name: "codemirror-editor",
+	defaults: {
+		mode: "javascript",
+		lineNumbers: true,
+		matchBrackets: true,
+		theme: "default"
 	},
-	$init:function(config){
+	$init: function (config) {
 		this.$view.innerHTML = "<textarea style='width:100%;height:100%;'></textarea>";
 		this._waitEditor = webix.promise.defer();
 		this.$ready.push(this._render_cm_editor);
 	},
-	_render_cm_editor:function(){
+	complex_types: {
+		php: {
+			mode: ["xml", "javascript", "css", "htmlmixed", "clike"]
+		},
+		htmlembedded: {
+			mode: ["xml", "javascript", "css", "htmlmixed"],
+			addon: ["mode/multiplex"]
+		},
+		htmlmixed: {
+			mode: ["xml", "javascript", "css"]
+		},
+		dockerfile: {
+			addon: ["mode/simple"]
+		}
+	},
+	_render_cm_editor: function () {
 
-		if (this.config.cdn === false){
+		if (this.config.cdn === false) {
 			this._render_when_ready;
 			return;
 		};
@@ -21,45 +36,47 @@ webix.protoUI({
 		var cdn = this.config.cdn || "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.48.4/";
 		// basic
 		var sources = [
-			cdn+"/codemirror.css",
-			cdn+"/codemirror.js"
+			cdn + "/codemirror.css",
+			cdn + "/codemirror.js"
 		];
 
 		// mode
 		var mode = this.config.mode ? this.config.mode : "javascript";
 		// complex modes based on others: htmlmixed, htmlembedded, php
-		if (mode.indexOf("html") === 0 || mode === "php") {
-			sources.push(cdn + "/mode/htmlmixed/htmlmixed.js");
-			sources.push(cdn + "/mode/xml/xml.js");
-			sources.push(cdn + "/mode/javascript/javascript.js");
-			sources.push(cdn + "/mode/css/css.js");
-			if (mode === "htmlembedded") {
-				sources.push(cdn + "/addon/mode/multiplex.js");
-				sources.push(cdn + "/mode/htmlembedded/htmlembedded.js");
-			} else if (mode === "php") {
-				sources.push(cdn + "/mode/clike/clike.js");
-				sources.push(cdn + "/mode/php/php.js");
-			}
-		} else
-			sources.push(cdn + "/mode/" + mode + "/" + mode + ".js");
+		var extras = this.complex_types[mode];
+		if (extras) {
+			if (extras["mode"]) {
+				extras["mode"].forEach(function (name) {
+					var path = "/mode/" + name + "/" + name + ".js";
+					sources.push(cdn + path);
+				});
+			};
+			if (extras["addon"]) {
+				extras["addon"].forEach(function (name) {
+					var path = "/addon/" + name + ".js";
+					sources.push(cdn + path);
+				});
+			};
+		};
+		sources.push(cdn + "/mode/" + mode + "/" + mode + ".js");
 
 		// theme
-		if (this.config.theme && this.config.theme !== "default"){
-			sources.push(cdn+"/theme/"+this.config.theme+".css")
+		if (this.config.theme && this.config.theme !== "default") {
+			sources.push(cdn + "/theme/" + this.config.theme + ".css")
 		};
 
 		// matchbrackets add-on
-		if(this.config.matchBrackets){
-			sources.push(cdn+"/addon/edit/matchbrackets.js")
+		if (this.config.matchBrackets) {
+			sources.push(cdn + "/addon/edit/matchbrackets.js")
 		};
 
 		webix.require(sources)
-		.then( webix.bind(this._render_when_ready, this) )
-		.catch(function(e){
-			console.log(e);
-		});		
+			.then(webix.bind(this._render_when_ready, this))
+			.catch(function (e) {
+				console.log(e);
+			});
 	},
-	_render_when_ready:function(){
+	_render_when_ready: function () {
 		this._editor = CodeMirror.fromTextArea(this.$view.firstChild, {
 			mode: this.config.mode,
 			lineNumbers: this.config.lineNumbers,
@@ -73,13 +90,13 @@ webix.protoUI({
 		if (this._focus_await)
 			this.focus();
 	},
-	_set_inner_size:function(){
+	_set_inner_size: function () {
 		if (!this._editor || !this.$width) return;
 
 		this._updateScrollSize();
-		this._editor.scrollTo(0,0); //force repaint, mandatory for IE
+		this._editor.scrollTo(0, 0); //force repaint, mandatory for IE
 	},
-	_updateScrollSize:function(){
+	_updateScrollSize: function () {
 		var box = this._editor.getWrapperElement();
 		var height = (this.$height || 0) + "px";
 
@@ -87,48 +104,48 @@ webix.protoUI({
 		box.style.width = (this.$width || 0) + "px";
 
 		var scroll = this._editor.getScrollerElement();
-		if (scroll.style.height != height){
+		if (scroll.style.height != height) {
 			scroll.style.height = height;
 			this._editor.refresh();
 		}
 	},
-	$setSize:function(x,y){
-		if (webix.ui.view.prototype.$setSize.call(this, x, y)){
+	$setSize: function (x, y) {
+		if (webix.ui.view.prototype.$setSize.call(this, x, y)) {
 			this._set_inner_size();
 		}
 	},
-	setValue:function(value){
-		if(!value && value !== 0)
+	setValue: function (value) {
+		if (!value && value !== 0)
 			value = "";
 
 		this.config.value = value;
-		if(this._editor){
+		if (this._editor) {
 			this._editor.setValue(value);
 			//by default - clear editor's undo history when setting new value
-			if(!this.config.preserveUndoHistory)
+			if (!this.config.preserveUndoHistory)
 				this._editor.clearHistory();
 			this._updateScrollSize();
 		}
 	},
-	getValue:function(){
-		return this._editor?this._editor.getValue():this.config.value;
+	getValue: function () {
+		return this._editor ? this._editor.getValue() : this.config.value;
 	},
-	focus:function(){
+	focus: function () {
 		this._focus_await = true;
 		if (this._editor)
 			this._editor.focus();
 	},
-	getEditor:function(waitEditor){
-		return waitEditor?this._waitEditor:this._editor;
+	getEditor: function (waitEditor) {
+		return waitEditor ? this._waitEditor : this._editor;
 	},
 	//undo, redo, etc
-	undo:function(){
+	undo: function () {
 		this._editor.undo();
 	},
-	redo:function(){
+	redo: function () {
 		this._editor.redo();
 	},
-	undoLength:function(){
+	undoLength: function () {
 		return this._editor.historySize().undo;
 	}
 }, webix.ui.view);
